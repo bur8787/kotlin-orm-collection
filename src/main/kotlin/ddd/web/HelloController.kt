dea316c83e229018fd0ba08bbb76ed1e2d092112
Takahiro Suzuki <bur8787@gmail.com>
2018-09-11 01:14:45 +0900

    fetch author

--- src/main/kotlin/ddd/web/HelloController.kt
+++ src/main/kotlin/ddd/web/HelloController.kt
@@ -1,12 +1,23 @@
 package ddd.web
 
+import com.example.jooq.tables.Authors.AUTHORS
+import org.jooq.DSLContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
-class HelloController {
+class HelloController(private val create: DSLContext) {
+
     @GetMapping("/")
-    fun hello() = HelloResponse(name = "suzuki")
+    fun hello(): HelloResponse {
+        return create.selectFrom(AUTHORS).where(AUTHORS.ID.eq(1)).fetchOne().let {
+            HelloResponse(
+                    name = it.name
+            )
+        }
+    }
+//    fun hello() = HelloResponse(name = "suzuki")
 }
 
 class HelloResponse(
