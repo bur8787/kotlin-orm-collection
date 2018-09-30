ad6b3bbd50e800faaf2c2d8fa1b5e5fca531d026
Takahiro Suzuki <bur8787@gmail.com>
2018-09-30 16:35:02 +0900

    remove jOOQ

--- src/main/kotlin/orm/repository/jooq/AuthorDao.kt
+++ src/main/kotlin/orm/repository/jooq/AuthorDao.kt
@@ -1,35 +1,12 @@
 package orm.repository.jooq
 
-import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import orm.domain.Author
-import orm.domain.Book
-import orm.jooq.Tables.AUTHORS
-import orm.jooq.Tables.BOOKS
-import orm.jooq.tables.records.AuthorsRecord
-import orm.jooq.tables.records.BooksRecord
 import orm.repository.AuthorRepository
 
 @Repository
-class AuthorDao(private val create: DSLContext) : AuthorRepository {
-
+class AuthorDao : AuthorRepository {
     override fun findById(id: Int): Author {
-        return create.select(AUTHORS.fields().asList())
-                .select(BOOKS.fields().asList())
-                .from(AUTHORS)
-                .join(BOOKS).on(AUTHORS.ID.eq(BOOKS.AUTHOR_ID))
-                .where(AUTHORS.ID.eq(id))
-                .fetchGroups(
-                        { r -> r.into(AUTHORS) },
-                        { r -> r.into(BOOKS) }
-                ).entries.first().let {
-            fromRow(it)
-        }
+        TODO()
     }
-
-    private fun fromRow(m: Map.Entry<AuthorsRecord, List<BooksRecord>>) = Author(
-            id = m.key.id,
-            name = m.key.name,
-            books = m.value.map { Book(id = it.id, name = it.name) }
-    )
 }
--- src/main/kotlin/orm/repository/jooq/TaskDao.kt
+++ src/main/kotlin/orm/repository/jooq/TaskDao.kt
@@ -1,19 +1,12 @@
 package orm.repository.jooq
 
-import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import orm.domain.Task
-import orm.jooq.Tables.TASKS
-import orm.jooq.tables.records.TasksRecord
 import orm.repository.TaskRepository
 
 @Repository
-class TaskDao(private val create: DSLContext) : TaskRepository {
+class TaskDao() : TaskRepository {
     override fun findByProjectId(projectId: Int): List<Task> {
-        return create.selectFrom(TASKS)
-                .where(TASKS.PROJECT_ID.eq(projectId))
-                .fetch { fromRow(it) }
+        TODO()
     }
-
-    private fun fromRow(r: TasksRecord) = Task(id = r.id, name = r.name, status = r.status, assignedTo = r.assignedTo)
 }
