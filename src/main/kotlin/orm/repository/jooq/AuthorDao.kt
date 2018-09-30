7c90cd120eecb12c7b57183a01c5d2ff145e05cc
Takahiro Suzuki <bur8787@gmail.com>
2018-10-01 00:47:59 +0900

    Revert "remove jOOQ"
    This reverts commit ad6b3bbd50e800faaf2c2d8fa1b5e5fca531d026.

--- src/main/kotlin/orm/repository/jooq/AuthorDao.kt
+++ src/main/kotlin/orm/repository/jooq/AuthorDao.kt
@@ -1,12 +1,35 @@
 package orm.repository.jooq
 
+import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import orm.domain.Author
+import orm.domain.Book
+import orm.jooq.Tables.AUTHORS
+import orm.jooq.Tables.BOOKS
+import orm.jooq.tables.records.AuthorsRecord
+import orm.jooq.tables.records.BooksRecord
 import orm.repository.AuthorRepository
 
 @Repository
-class AuthorDao : AuthorRepository {
+class AuthorDao(private val create: DSLContext) : AuthorRepository {
+
     override fun findById(id: Int): Author {
-        TODO()
+        return create.select(AUTHORS.fields().asList())
+                .select(BOOKS.fields().asList())
+                .from(AUTHORS)
+                .join(BOOKS).on(AUTHORS.ID.eq(BOOKS.AUTHOR_ID))
+                .where(AUTHORS.ID.eq(id))
+                .fetchGroups(
+                        { r -> r.into(AUTHORS) },
+                        { r -> r.into(BOOKS) }
+                ).entries.first().let {
+            fromRow(it)
+        }
     }
+
+    private fun fromRow(m: Map.Entry<AuthorsRecord, List<BooksRecord>>) = Author(
+            id = m.key.id,
+            name = m.key.name,
+            books = m.value.map { Book(id = it.id, name = it.name) }
+    )
 }
--- src/main/kotlin/orm/repository/jooq/ProjectDao.kt
+++ src/main/kotlin/orm/repository/jooq/ProjectDao.kt
@@ -1,17 +1,43 @@
 package orm.repository.jooq
 
+import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import orm.domain.Project
+import orm.jooq.Tables.*
+import orm.jooq.tables.records.ProjectsRecord
+import orm.jooq.tables.records.UsersRecord
 import orm.repository.ProjectRepository
 
 @Repository
-class ProjectDao : ProjectRepository {
+class ProjectDao(private val create: DSLContext) : ProjectRepository {
 
     override fun create(project: Project): Project {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun findByUserId(userId: Int): List<Project> {
-        TODO()
+        return create.select(PROJECTS.fields().asList())
+                .select(USERS.fields().asList())
+                .from(PROJECTS)
+                .join(USER_TO_PROJECT).on(PROJECTS.ID.eq(USER_TO_PROJECT.PROJECT_ID))
+                .join(USERS).on(USERS.ID.eq(USER_TO_PROJECT.USER_ID))
+                .where(USERS.ID.eq(userId))
+                .fetchGroups(
+                        { r -> r.into(USERS) },
+                        { r -> r.into(PROJECTS) }
+                ).entries.first().let {
+            fromRow(it)
+        }
+    }
+
+    private fun fromRow(m: Map.Entry<UsersRecord, List<ProjectsRecord>>): List<Project> {
+        return m.value.map {
+            Project(
+                    id = it.id,
+                    title = it.name,
+                    subTitle = "",
+                    logoUrl = ""
+            )
+        }
     }
 }
